d9998218e9dca01c628bb0e207c17ae4a6c0e5f8
yellow-egg-a <yellow_egg_a@naver.com>
2023-07-09 18:41:51 +0900

    feat: 5. 회원 관리 예제 - 웹 MVC 개발 - 회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -37,7 +37,12 @@ public class MemberController {
 
         return "redirect:/";
     }
-    
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
